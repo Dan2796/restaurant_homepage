840baaa903cb8f58bf4b53dcca2d9433d74f1bc1
Dan Snow <danjsnow@outlook.com>
2023-05-22 16:31:24 +0100

    Updates text, particularly for the menu page

--- src/components/home_tab.js
+++ src/components/home_tab.js
@@ -11,9 +11,9 @@ export default function homeTab() {
   homeContent.appendChild(para4);
 
   para1.textContent = 'We believe that good food is the foundation of a happy life, and we express that belief in every dish we serve.';
-  para2.textContent = 'We are located in Covent Gardens, but join us for an evening and your pallete will be transported to the most beautiful province in Italy. Tuscany may be famous for it\'s wine, but no true Italian would waste a good wine on bad food; our menu ensures that you can enjoy the finest Chianti with the best hand-made Italian fare on offer in London.';
-  para3.textContent = 'So join us for your next celebration, your next date night, your next family reunion, and relax for a few hours in the comfort of a cozy Tuscan dining experience.';
-  para4.textContent = 'At Ristorante Rustico Toscano, "Buono appetito" is not a hope, it\'s a promise.';
+  para2.textContent = 'We are located in Covent Gardens, but join us for an evening and your pallete will be transported to the most beautiful province in Italy. Tuscany may be famous for it\'s wine, but a good wine deserves even better food. Our menu ensures that you can enjoy the finest Chianti with a plate of the best handmade Italian fare available in London.';
+  para3.textContent = 'So join us for your next celebration, your next date night, or your next family reunion, and relax for a few hours in the comfort of a true Tuscan dining experience.';
+  para4.textContent = 'At Ristorante Rustico Toscano, we don\'t just wish you "Buono appetito", we promise it.';
 
   return homeContent;
 }
--- src/components/menu_tab.js
+++ src/components/menu_tab.js
@@ -2,18 +2,24 @@ export default function menuTab() {
   const menuContent = document.createElement('div');
   const para1 = document.createElement('p');
   const para2 = document.createElement('p');
-  const para3 = document.createElement('p');
-  const para4 = document.createElement('p');
-
+  const para3 = document.createElement('span');
+  const link = document.createElement('a');
+  const para4 = document.createElement('span');
+  const para5 = document.createElement('p');
   menuContent.appendChild(para1);
   menuContent.appendChild(para2);
   menuContent.appendChild(para3);
+  menuContent.appendChild(link);
   menuContent.appendChild(para4);
+  menuContent.appendChild(para5);
 
-  para1.textContent = 'Italian food item 1 ';
-  para2.textContent = 'More Italian food';
-  para3.textContent = 'Some other Italian food';
-  para4.textContent = 'Some English cuisine... just kidding, this will also be Italian food.';
+  para1.textContent = 'We offer the finest fare that Italy has to offer.';
+  para2.textContent = 'If you\'re not feeling too hungry, try a bowl of pasta or something else from our Primo courses. If you are in need of something more filling, a range of traditional dishes await you in the Secondi section. Or for a true Italian experience, try one of each, and also see our Starters, our Desserts, and our fine selection of Grappa.';
+  para3.textContent = 'Our full menu can be downloaded ';
+  link.setAttribute('href', 'https://en.wikipedia.org/wiki/Tuscany');
+  link.textContent = 'here';
+  para4.textContent = '.';
+  para5.textContent = 'If you have specific dietary requirements or concerns, please let us know in advance and our chefs will happily accomodate your needs.';
 
   return menuContent;
 }
